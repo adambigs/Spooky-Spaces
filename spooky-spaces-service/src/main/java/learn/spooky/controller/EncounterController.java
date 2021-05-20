46fc5a721e6c726f028eb9828722bbab8165e36c
adam bigelow <65466512+adambigs@users.noreply.github.com>
2021-05-20 09:27:33 -0500

    encounter controller changes

--- spooky-spaces-service/src/main/java/learn/spooky/controller/EncounterController.java
+++ spooky-spaces-service/src/main/java/learn/spooky/controller/EncounterController.java
@@ -1,10 +1,12 @@
 package learn.spooky.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/encounter")
 public class EncounterController {
 
 
