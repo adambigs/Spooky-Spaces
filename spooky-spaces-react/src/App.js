6b8fe9e8e4497bfee09d5a07f7124f6da68b3c0a
Christine <ryvian@gmail.com>
2021-05-21 14:24:54 -0500

    Navbar added

--- spooky-spaces-react/src/App.js
+++ spooky-spaces-react/src/App.js
@@ -1,37 +1,31 @@
-import { 
+import {
   BrowserRouter as Router,
-  Link,
   Switch,
   Route,
-  Redirect} from 'react-router-dom';
-import { useState } from 'react';  
-import Home from './components/Home'; 
-import About from './components/static/About';
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/static/About";
+import NotFound from "./components/static/NotFound";
 
 function App() {
   return (
     <div className="App">
-    <div className="container">  
-    <Router>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-          </ul>
-        
+        <Router>
           <Switch>
             <Route exact path="/">
-              <Home />        
+              <Home />
             </Route>
             <Route path="/about">
               <About />
             </Route>
-          </Switch> 
-        </Router> 
-</div>
+            <Route path="/allspaces">
+            
+            </Route>
+            <Route path="*">
+            <NotFound />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
