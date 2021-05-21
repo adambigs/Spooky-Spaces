c4b4f9fc9d6b1b1e8a6334736243c63731dc7ad4
adam bigelow <65466512+adambigs@users.noreply.github.com>
2021-05-21 13:02:30 -0500

    react google maps api

---
+++ spooky-spaces-react/src/components/EditComment.js
@@ -0,0 +1,6 @@
+function EditComment(){
+
+
+}
+
+export default EditComment;
---
+++ spooky-spaces-react/src/components/Home.js
@@ -0,0 +1,11 @@
+import MapContainer from './MapContainer';
+
+function Home(){
+
+    return (
+        <MapContainer />
+    );
+
+}
+
+export default Home;
