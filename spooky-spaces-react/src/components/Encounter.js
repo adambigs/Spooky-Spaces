bec2b9d7fba588d8494d61760ab930de352ea6ed
adam bigelow <65466512+adambigs@users.noreply.github.com>
2021-05-24 15:50:31 -0500

    fixed bbig bugs

--- spooky-spaces-react/src/components/Encounter.js
+++ spooky-spaces-react/src/components/Encounter.js
@@ -1,9 +1,16 @@
+import CommentList from "./CommentList";
+
 function Encounter({encounterId, description}){
 
     return(
+        <>
         <div className="list-group-item text-center">
         {description} 
         </div>
+        <div>
+            <CommentList encounterId={encounterId}/>
+        </div>
+        </>
     );
 }
 
