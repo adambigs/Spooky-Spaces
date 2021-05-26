fa0a9c8873b3bf629ca2a64277ea6ff160ecd0ff
Christine <ryvian@gmail.com>
2021-05-26 10:59:09 -0500

    stuff

--- spooky-spaces-react/src/components/About.js
+++ spooky-spaces-react/src/components/About.js
@@ -7,14 +7,14 @@ function About() {
       <>
       <Nav />
       <div className="container">
-      <h2>
+      <p>
           This app is for tourists or locals of Milwaukee that are interested in ghost encounters/hauntings. 
           With so much rich history in Milwaukee, and the gaining popularity of ghost sightings, there seems to be a growing desire to find these encounters. 
           The difficulty of being a ghost hunter/ enthusiast is going to a new city and knowing where to begin. 
           How does someone find something that may or may not exist? 
           A scary good network of other ghost hunters, that’s how. 
           We have devised an app to solve that problem, and simultaneously generate a new scope of tourism revenue to our town, a ghost map.
-      </h2>
+      </p>
       </div>
       </>
     )
